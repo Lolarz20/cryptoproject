5ce660d4fc5e6a9823ab9c895823a51015bd8cdf
Lolarz20 <lolarz@protonmail.com>
2024-04-10 04:21:47 +0200

    1

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cryptoproject/functions.dart';
+import 'package:custom_sliding_segmented_control/custom_sliding_segmented_control.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:marquee/marquee.dart';
 
 class AdminPage extends StatefulWidget {
   const AdminPage({super.key});
@@ -19,19 +22,19 @@ class _AdminPageState extends State<AdminPage> {
     return Scaffold(
       body: Column(
         children: [
-          SlidingBarMoving(),
+          const SlidingBarMoving(),
           Padding(
             padding: const EdgeInsets.all(20),
             child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                ManageBot(),
+                const ManageBot(),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SummaryWidget(),
+                    const SummaryWidget(),
                     SizedBox(height: height*0.025),
-                    ChartWidget()
+                    const ChartWidget()
                   ],
                 )
               ],
@@ -51,20 +54,34 @@ class SlidingBarMoving extends StatefulWidget {
 }
 
 class _SlidingBarMovingState extends State<SlidingBarMoving> {
-  final List<String> _images = const [
-    'BTC',
-    'ETH',
-    'ADA',
-    'XRP',
-    'SAGA',
-    'USDT',
-  ];
+
+  final List<String> _images = [];
+  final List<String> _symbols = [];
+  final List<String> _pnl = [];
 
   final CarouselController _carouselController = CarouselController();
+
+  void getPositions() async {
+    List<Map> list = [];
+    list = await getOpenFuturesPositions();
+    for(var i in list){
+      for(var x in i.keys){
+        _symbols.add(x);
+      }
+      for(var y in i.values){
+        _pnl.add(y);
+      }
+      for(var z in i.keys){
+        _images.add(await getCryptoIconsUrl(z.toString().replaceAll('USDT', '')));
+      }
+    }
+    setState(() {});
+  }
 
   @override
   void initState() {
     super.initState();
+    getPositions();
     // Add this to start the animation
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _carouselController.nextPage(
@@ -77,7 +94,7 @@ class _SlidingBarMovingState extends State<SlidingBarMoving> {
     var width = MediaQuery.of(context).size.width;
     return Container(
       color: Colors.white,
-      height: 50,
+      height: 80,
       width: width,
       child: SingleChildScrollView(
         physics: const BouncingScrollPhysics(
@@ -91,20 +108,13 @@ class _SlidingBarMovingState extends State<SlidingBarMoving> {
               options: CarouselOptions(
                   pageSnapping: false,
                   height: 50,
-                  viewportFraction: 150 / MediaQuery.of(context).size.width,
+                  viewportFraction: 200 / MediaQuery.of(context).size.width,
                   // Add this
                   onPageChanged: (_, __) {
                     _carouselController.nextPage(
                         duration: const Duration(milliseconds: 2000));
                   }),
-              items: _images
-                  .map((image) => ClipRRect(
-                  borderRadius: BorderRadius.circular(16.0),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(image)
-                  )))
-                  .toList(),
+              items: List.generate(_symbols.length, (index) => ContainerForTicker(icon: _images[index], symbol: _symbols[index], pnl: _pnl[index]))
             ),
           ],
         ),
@@ -112,6 +122,33 @@ class _SlidingBarMovingState extends State<SlidingBarMoving> {
     );
   }
 }
+
+class ContainerForTicker extends StatelessWidget {
+  final String icon;
+  final String symbol;
+  final String pnl;
+  const ContainerForTicker({super.key, required this.icon, required this.symbol, required this.pnl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            CircleAvatar(foregroundImage: NetworkImage(icon)),
+            Text(symbol),
+            Text(double.parse(pnl).toStringAsFixed(2))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+enum Bots {futures, spot}
 
 class ManageBot extends StatefulWidget {
   const ManageBot({super.key});
@@ -121,12 +158,79 @@ class ManageBot extends StatefulWidget {
 }
 
 class _ManageBotState extends State<ManageBot> {
+
+  final List<String> symbols = [
+    'Bitcoin',
+    'Ethereum',
+    'BNB',
+    'Solana',
+    'XRP',
+    'Dogecoin',
+    'Cardano',
+    'Avalanche',
+    'Shiba Inu',
+    'Bitcoin Cash',
+    'Polkadot',
+    'Tron',
+    'Chainlink',
+    'Polygon',
+    'NEAR Protocol',
+    'Internet Computer',
+    'Litecoin',
+    'Uniswap',
+    'Dai',
+    'Aptos',
+    'Ethereum Classic',
+    'Stacks',
+    'Filecoin',
+    'Cosmos',
+    'Arbitrum',
+    'Stellar',
+    'Immutable',
+    'Render',
+    'Hedera',
+    'VeChain',
+    'The Graph',
+    'Maker',
+    'Optimism',
+    'Injective',
+    'Theta Network',
+    'Fantom',
+    'THORChain',
+    'Monero',
+    'Aave',
+    'Flow',
+    'NEO',
+    'EOS',
+  ];
+
+  bool loadingSymbol = false;
+
+  void createListOfSymbols() async {
+    List<String> list1 = await getCryptoIconsUrls(listOfSymbol);
+    List<double> list2 = (await getCryptoPrices(listOfSymbol)).values.toList();
+    List<String> list3 = listOfSymbol;
+    List<String> list4 = symbols;
+    setState(() {
+      list = List.generate(listOfSymbol.length, (index) => SymbolWidget(image: list1[index], price: list2[index], symbol: list3[index], sign: list4[index]));
+      loadingSymbol = true;
+    });
+  }
+
+  @override
+  void initState() {
+    createListOfSymbols();
+    super.initState();
+  }
+
+  List<SymbolWidget> list = [];
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.sizeOf(context).width;
     final height = MediaQuery.sizeOf(context).height;
     return Card(
-      elevation: 10,
+      elevation: 1,
       color: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
@@ -136,13 +240,133 @@ class _ManageBotState extends State<ManageBot> {
         height: height*0.8,
         child: Column(
           children: [
-
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CustomSlidingSegmentedControl<Bots>(
+                initialValue: Bots.futures,
+                isStretch: true,
+                children: const {
+                  Bots.futures: Text(
+                    'Futures',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Russo',
+                        color: Colors.blue,
+                        fontSize: 9),
+                  ),
+                  Bots.spot: Text(
+                    'Spot',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Russo',
+                        color: Colors.blue,
+                        fontSize: 9),
+                  ), //1
+                },
+                innerPadding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                thumbDecoration: BoxDecoration(
+                  color: CupertinoColors.systemGrey6,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                onValueChanged: (v) async {
+                    if(v==Bots.spot){
+                      //getSpotSymbols();
+                      print(await getSpotAccountBalance());
+                    } else {
+                      print(await getFuturesSymbolsSortedByVolume());
+                      print(await getFuturesAccountBalance());
+                    }
+                },
+              ),
+            ),
+            loadingSymbol==false? const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircularProgressIndicator(color: CupertinoColors.systemGrey),
+                Text('loading symbols...', style: TextStyle(color: Colors.grey))
+              ]
+            ) : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: height*0.7,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: list,
+                    ),
+                  )
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class SymbolWidget extends StatelessWidget {
+  final String image;
+  final double price;
+  final String symbol;
+  final String sign;
+  const SymbolWidget({super.key, required this.image, required this.price, required this.symbol, required this.sign});
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: height*0.075,
+        width: width*0.01,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          border: Border.all(color: Colors.blue, width: 5)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CircleAvatar(foregroundImage: NetworkImage(image)),
+              Text(sign, style: const TextStyle(fontFamily: 'Russo', color: Colors.blueAccent, fontSize: 18)),
+              Text(symbol, style: const TextStyle(fontFamily: 'Russo', color: Colors.grey, fontSize: 12)),
+              Text('\$$price', style: const TextStyle(fontFamily: 'Russo', color: Colors.grey))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class AddAvailableSymbols extends StatefulWidget {
+  final List<String> symbols;
+  const AddAvailableSymbols({super.key, required this.symbols});
+
+  @override
+  State<AddAvailableSymbols> createState() => _AddAvailableSymbolsState();
+}
+
+class _AddAvailableSymbolsState extends State<AddAvailableSymbols> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 
 class SummaryWidget extends StatefulWidget {
   const SummaryWidget({super.key});
@@ -152,12 +376,35 @@ class SummaryWidget extends StatefulWidget {
 }
 
 class _SummaryWidgetState extends State<SummaryWidget> {
+
+  double pnl = 0.0;
+  double moneyOnFutures = 0.0;
+
+  void getPNL() async {
+    Timer.periodic(const Duration(seconds: 1), (timer) async {
+      pnl = double.parse(await getFuturesPNL());
+      setState(() {});
+    });
+  }
+
+  void getFuturesAccount() async {
+    moneyOnFutures = double.parse(await getFuturesAccountBalance());
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getPNL();
+    getFuturesAccount();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.sizeOf(context).width;
     final height = MediaQuery.sizeOf(context).height;
     return Card(
-      elevation: 10,
+      elevation: 1,
       color: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
@@ -166,8 +413,42 @@ class _SummaryWidgetState extends State<SummaryWidget> {
         width: width*0.65,
         height: height*0.32,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+            Padding(padding: const EdgeInsets.all(25), child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    const Text('Profit and Loss (PNL):', style: TextStyle(fontWeight: FontWeight.w200, fontSize: 25)),
+                    Text('${pnl.isNegative? pnl : '+$pnl'}', style: TextStyle(color: pnl.isNegative? Colors.red : Colors.green, fontSize: 40, fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+            )),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        const Text('Futures Balance:', style: TextStyle(fontWeight: FontWeight.w200, fontSize: 20)),
+                        Text('\$$moneyOnFutures', style: const TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
@@ -188,7 +469,7 @@ class _ChartWidgetState extends State<ChartWidget> {
     final width = MediaQuery.sizeOf(context).width;
     final height = MediaQuery.sizeOf(context).height;
     return Card(
-      elevation: 10,
+      elevation: 1,
       color: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
@@ -196,12 +477,205 @@ class _ChartWidgetState extends State<ChartWidget> {
       child: SizedBox(
         width: width*0.65,
         height: height*0.45,
-        child: Column(
+        child: const Column(
+          children: [
+            LineChartSample2()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BotCreationWidget extends StatefulWidget {
+  const BotCreationWidget({super.key});
+
+  @override
+  State<BotCreationWidget> createState() => _BotCreationWidgetState();
+}
+
+class _BotCreationWidgetState extends State<BotCreationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    final height = MediaQuery.sizeOf(context).height;
+    return Card(
+      elevation: 1,
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: SizedBox(
+        width: width*0.65,
+        height: height*0.7,
+        child: const Column(
           children: [
 
           ],
         ),
       ),
+    );
+  }
+}
+
+class LineChartSample2 extends StatefulWidget {
+  const LineChartSample2({super.key});
+
+  @override
+  State<LineChartSample2> createState() => _LineChartSample2State();
+}
+
+class _LineChartSample2State extends State<LineChartSample2> {
+  List<Color> gradientColors = [
+    Colors.cyan,
+    Colors.blue,
+  ];
+
+  bool showAvg = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.sizeOf(context).width;
+    final height = MediaQuery.sizeOf(context).height;
+    return SizedBox(
+      width: width*0.64,
+      height: height*0.44,
+      child: AspectRatio(
+        aspectRatio: 1.70,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            right: 18,
+            left: 12,
+            top: 24,
+            bottom: 12,
+          ),
+          child: LineChart(
+            mainData()
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+      color: Colors.grey
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 2:
+        text = const Text('MAR', style: style);
+        break;
+      case 5:
+        text = const Text('JUN', style: style);
+        break;
+      case 8:
+        text = const Text('SEP', style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: text,
+    );
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 15,
+      color: Colors.grey
+    );
+    String text;
+    switch (value.toInt()) {
+      case 1:
+        text = '10K';
+        break;
+      case 3:
+        text = '30k';
+        break;
+      case 5:
+        text = '50k';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text, style: style, textAlign: TextAlign.left);
+  }
+
+  LineChartData mainData() {
+    return LineChartData(
+      baselineX: 0,
+      baselineY: 0,
+      gridData: FlGridData(show: false),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        topTitles: const AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            reservedSize: 30,
+            interval: 1,
+            getTitlesWidget: bottomTitleWidgets,
+          ),
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: 1,
+            getTitlesWidget: leftTitleWidgets,
+            reservedSize: 42,
+          ),
+        ),
+      ),
+      borderData: FlBorderData(
+        show: false,
+      ),
+      minX: 0,
+      maxX: 11,
+      minY: 0,
+      maxY: 6,
+      lineBarsData: [
+        LineChartBarData(
+          spots: const [
+            FlSpot(0, 3),
+            FlSpot(2.6, 2),
+            FlSpot(4.9, 5),
+            FlSpot(6.8, 3.1),
+            FlSpot(8, 4),
+            FlSpot(9.5, 3),
+            FlSpot(11, 4),
+          ],
+          isCurved: true,
+          gradient: LinearGradient(
+            colors: gradientColors,
+          ),
+          barWidth: 10,
+          isStrokeCapRound: true,
+          dotData: const FlDotData(
+            show: false,
+          ),
+          belowBarData: BarAreaData(
+            show: true,
+            gradient: LinearGradient(
+              colors: gradientColors
+                  .map((color) => color.withOpacity(0.3))
+                  .toList(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/functions.dart
@@ -0,0 +1,287 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:http/http.dart' as http;
+
+String apiKey = '097941fec6ce719b0f177ce6f614d1b050c3dd9aa7cebccd6d28ada3b205acd0';
+String apiSecret = '7b033f039d6158bad616cf1f9dd19ede75c697b0ba72cce2dd4e783a86ce35d6';
+
+Future<List<String>> getFuturesSymbols() async {
+  const String url = 'https://fapi.binance.com/fapi/v1/exchangeInfo';
+  List<String> symbolsList = [];
+
+  try {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final symbols = data['symbols'] as List<dynamic>;
+
+      // Dodawanie symboli do listy
+      for (var symbol in symbols) {
+        symbolsList.add(symbol['symbol']);
+      }
+    } else {
+      print('Failed to load futures symbols. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+
+  return symbolsList;
+}
+
+Future<List<String>> getFuturesSymbolsSortedByVolume() async {
+  const String url = 'https://fapi.binance.com/fapi/v1/ticker/24hr';
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> tickers = jsonDecode(response.body);
+    // Sortowanie według wolumenu obrotu (konwersja string na double)
+    tickers.sort((a, b) => double.parse(b['quoteVolume']).compareTo(double.parse(a['quoteVolume'])));
+    // Pobieranie symboli
+    List<String> sortedSymbols = tickers.map<String>((ticker) => ticker['symbol'].toString()).toList();
+    // Zwracanie pierwszych 30 symboli po sortowaniu
+    return sortedSymbols.take(100).toList();
+  } else {
+    throw Exception('Failed to load futures symbols');
+  }
+}
+
+Future<List<String>> getSpotSymbols() async {
+  const String url = 'https://api.binance.com/api/v3/exchangeInfo';
+  List<String> symbolsList = [];
+
+  try {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final symbols = data['symbols'] as List<dynamic>;
+
+      // Dodawanie symboli do listy
+      for (var symbol in symbols) {
+        symbolsList.add(symbol['symbol']);
+      }
+    } else {
+      print('Failed to load spot symbols. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
+
+  return symbolsList;
+}
+
+String generateSignature(String data) {
+  var key = utf8.encode(apiSecret);
+  var bytes = utf8.encode(data);
+
+  var hmacSha256 = Hmac(sha256, key); // HMAC-SHA256
+  var digest = hmacSha256.convert(bytes);
+
+  return digest.toString();
+}
+
+Future<String> getFuturesAccountBalance() async {
+  String baseUrl = 'https://testnet.binancefuture.com';
+  String endPoint = '/fapi/v2/account';
+  int timestamp = DateTime.now().millisecondsSinceEpoch;
+
+  String queryString = 'timestamp=$timestamp';
+  String signature = generateSignature(queryString);
+
+  String url = '$baseUrl$endPoint?$queryString&signature=$signature';
+
+  var headers = {
+    'X-MBX-APIKEY': apiKey,
+  };
+
+  var response = await http.get(Uri.parse(url), headers: headers);
+
+  if (response.statusCode == 200) {
+    // Return the body of the response
+    return (double.parse(jsonDecode(response.body)['totalWalletBalance'].toString()).roundToDouble()).toString();
+  } else {
+    return 'Error fetching futures account info: ${response.body}';
+  }
+}
+
+Future<String> getFuturesPNL() async {
+  String baseUrl = 'https://testnet.binancefuture.com';
+  String endPoint = '/fapi/v2/account';
+  int timestamp = DateTime.now().millisecondsSinceEpoch;
+
+  String queryString = 'timestamp=$timestamp';
+  String signature = generateSignature(queryString);
+
+  String url = '$baseUrl$endPoint?$queryString&signature=$signature';
+
+  var headers = {
+    'X-MBX-APIKEY': apiKey,
+  };
+
+  var response = await http.get(Uri.parse(url), headers: headers);
+
+  if (response.statusCode == 200) {
+    // Return the body of the response
+    return (double.parse(jsonDecode(response.body)['totalUnrealizedProfit']).toStringAsFixed(2));
+  } else {
+    return 'Error fetching futures account info: ${response.body}';
+  }
+}
+
+const List<String> listOfSymbol = [
+  'BTC',
+  'ETH',
+  'BNB',
+  'SOL',
+  'XRP',
+  'DOGE',
+  'ADA',
+  'AVAX',
+  'SHIB',
+  'BCH',
+  'DOT',
+  'TRX',
+  'LINK',
+  'MATIC',
+  'NEAR',
+  'ICP',
+  'LTC',
+  'UNI',
+  'DAI',
+  'APT',
+  'ETC',
+  'STX',
+  'FIL',
+  'ATOM',
+  'ARB',
+  'XLM',
+  'IMX',
+  'RNDR',
+  'HBAR',
+  'VET',
+  'GRT',
+  'MKR',
+  'OP',
+  'INJ',
+  'THETA',
+  'FTM',
+  'RUNE',
+  'XMR',
+  'AAVE',
+  'FLOW',
+  'NEO',
+  'EOS',
+];
+
+Future<String> getSpotAccountBalance() async {
+  String baseUrl = 'https://testnet.binance.vision/api';
+  String endPoint = '/api/v3/account';
+  int timestamp = DateTime.now().millisecondsSinceEpoch;
+
+  String queryString = 'timestamp=$timestamp';
+  String signature = generateSignature(queryString);
+
+  String url = '$baseUrl$endPoint?$queryString&signature=$signature';
+
+  var headers = {
+    'X-MBX-APIKEY': apiKey,
+  };
+
+  var response = await http.get(Uri.parse(url), headers: headers);
+
+  if (response.statusCode == 200) {
+    // Return the body of the response
+    return response.body;
+  } else {
+    return 'Error fetching spot account info: ${response.body}';
+  }
+}
+
+Future<List<String>> getCryptoIconsUrls(List<String> symbols) async {
+  List<String> urls = [];
+  final storageRef = FirebaseStorage.instance.ref();
+
+  for (String symbol in symbols) {
+    String iconPath = 'CRYPTOICONS/$symbol.png';
+    String url = await storageRef.child(iconPath).getDownloadURL();
+    urls.add(url);
+  }
+  return urls;
+}
+
+Future<String> getCryptoIconsUrl(String symbol) async {
+  final storageRef = FirebaseStorage.instance.ref();
+  String iconPath = 'CRYPTOICONS/$symbol.png';
+  String url = await storageRef.child(iconPath).getDownloadURL();
+
+  return url;
+}
+
+Future<Map<String, double>> getCryptoPrices(List<String> symbols) async {
+  Map<String, double> prices = {};
+
+  for (String symbol in symbols) {
+    final response = await http.get(Uri.parse('https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT'));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final String symbol = data['symbol'];
+      final double price = double.parse(data['price']);
+      prices[symbol] = price;
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  return prices;
+}
+
+class FuturePosition {
+  final String symbol;
+  final double positionAmt; // Ilość
+  final double unrealizedProfit; // Niezrealizowany zysk
+
+  FuturePosition({required this.symbol, required this.positionAmt, required this.unrealizedProfit});
+
+  factory FuturePosition.fromJson(Map<String, dynamic> json) {
+    return FuturePosition(
+      symbol: json['symbol'],
+      positionAmt: double.parse(json['positionAmt']),
+      unrealizedProfit: double.parse(json['unrealizedProfit']),
+    );
+  }
+}
+
+Future<List<Map<String, String>>> getOpenFuturesPositions() async {
+  const baseUrl = 'https://testnet.binancefuture.com';
+  const endpoint = '/fapi/v2/positionRisk';
+  final timestamp = DateTime.now().millisecondsSinceEpoch;
+  final queryParams = 'timestamp=$timestamp';
+  final signature = Hmac(sha256, utf8.encode(apiSecret)).convert(utf8.encode(queryParams));
+  final url = Uri.parse('$baseUrl$endpoint?$queryParams&signature=$signature');
+
+  try {
+    final response = await http.get(url, headers: {
+    'X-MBX-APIKEY': apiKey,
+    },);
+    if (response.statusCode == 200) {
+      List<dynamic> positions = json.decode(response.body);
+      List<Map<String, String>> list = [];
+      for (var position in positions) {
+        if(double.parse(position['unRealizedProfit'])!=0.00000000) {
+          list.add({position['symbol']:position['unRealizedProfit']});
+        }
+      }
+      return list;
+    } else {
+      print('Błąd podczas pobierania danych o pozycji: ${response.body}');
+      return [{}];
+    }
+  } catch (e) {
+    print('Wystąpił błąd: $e');
+    return [{}];
+  }
+}
